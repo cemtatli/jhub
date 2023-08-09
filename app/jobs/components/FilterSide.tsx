9d05778d03bf343ce34c0798187bea394852afbb
cemtatli <cemtatli3535@gmail.com>
2023-08-09 17:44:37 +0300

    FIX

--- app/jobs/components/FilterSide.tsx
+++ app/jobs/components/FilterSide.tsx
@@ -5,7 +5,7 @@ import SelectDate from './SelectDate'
 const FilterSide = () => {
   return (
     <aside className='w-72 flex flex-col h-fit-screen gap-5 pt-5'>
-      <SelectCity />
+      {/*     <SelectCity /> */}
       <SelectDate />
     </aside>
   )
